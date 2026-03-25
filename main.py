22dbb6006c1c9855083b5328455665851b4e5559
Giorgia Trudu <g.trudu17@studenti.unica.it>
2026-03-25 09:03:31 +0100

    linguaggio html

--- main.py
+++ main.py
@@ -5,31 +5,19 @@ app = FastAPI ()
 #cioò all'interno delle parentesi della funzione endpoint, sono i parametri d'ingresso della richiesta
 #si possono inserire anche parametri di default e quindi facoltativi
 #TYPING--->in FASTAPI rispetto PYTHON è reso obbligatorio in runtime, e segnala errore in presenza di errore
-def hello_world(q: str, sort: bool = False) -> dict[str, bool | str]:
-    return {"q": q, "sort": sort}
+def home():
+    """Renders the home page"""
+    html = """
+    <!DOCTYPE html>
+    <html>
+    <body> 
+    <h1> Hello World! </h1>
+    <p> This is a simple FASTAPI app.</p>
+    </body>
+    </html>
+    """
 
-@app.get("/home")
-def homepage ():
-    return "This is the homepage"
+    return html
 
-#endpoint
-#anche devo usare il typing e specificare che username deve essere una stringa
-@app.get("/{username}")
-def username_webpage (username: str):
-    return f"This is the webpage of user {username}"
-#f sta er format, se non lo mettessi l'output risulterebbe una semplice stringa
-#mentre con la f, appena trova le parentesi grafe, prende il parametro all'interno e lo stampa
-
-#endpoint
-#devo usare il typing e specificare che username deve essere una stringa
-#username è parametrico e dev'essere una stringa, orders è una pagina fissa, e order_id è parametrico e dev'essere un intero
-
-#N.B. POSSO ANCHE COMBINARE PARAMETRI DI DIVERSE APPLICAZIONI, E L'ORDINE NON CONTA PERCHE' FASTAPI RICONOSCE IN BASE AI NOMI
-#L'UNICO ORDINE DA RISPETTARE E' CHE IN PYTHON I PARAMETRI OPZIONALI DEVONO ESSERE INSERITI PER FORZA PER ULTIMI
-@app.get("/{username}/orders/{order_id}")
-def orders_webpage (username: str, order_id: int, q: int, sort:bool = False):
-    return f"This is the order {order_id} of user {username}. q: {q}, Sorted: {sort}"
-#f sta er format, se non lo mettessi l'output risulterebbe una semplice stringa
-#mentre con la f, appena trova le parentesi grafe, prende il parametro all'interno e lo stampa
 
 
