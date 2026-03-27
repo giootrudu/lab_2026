4616da40fe86a9b5656ec320079f0f0d4577be6a
Giorgia Trudu <g.trudu17@studenti.unica.it>
2026-03-27 10:16:06 +0100

    "NUOVE APPLICAZIONI PER LA GESTIONE DEL FORM PRODOTTI"

--- main.py
+++ main.py
@@ -24,11 +24,30 @@ def home(request: Request ):
     #TERZO PARAMETRO DELLA TEMPLATE RESPONSE
 
 
-    return templates.TemplateResponse(request = request, name = "home.html", context = {"test": "Welcome to the store"})
+    return templates.TemplateResponse(request = request,
+                                      name = "home.html",
+                                      context = {"text": "Welcome to the store"}
+                                      )
 
 @app.get ("/products", response_class = HTMLResponse)
 def products (request: Request):
-    return templates.TemplateResponse(request = request, name = "products.html", context = {"product_list": product_list})
+    return templates.TemplateResponse(request = request,
+                                      name = "products.html",
+                                      context = {"product_list": product_list})
 
+@app.get ("/products_form", response_class = HTMLResponse)
+def products_form (request: Request):
+        return templates.TemplateResponse(request = request,
+                                          name = "products_form.html"
+                                          )
+
+#END POINT CHE RACCOGLIE I DATI DEL FORM
+@app.get ("/insert_product")
+def insert_product (name: str | None = None,
+                    price: float | None = None,
+                    location: str | None = None):
+    product = {"name": name, "price": price, "location": location}
+    product_list.append(product)
+    return "Product added successfully"
 
 
