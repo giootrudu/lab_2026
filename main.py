6bd4430a4f9c40f2c9fea3ed30c888b97e575f85
Giorgia Trudu <g.trudu17@studenti.unica.it>
2026-03-27 08:53:53 +0100

    MAIN PER SITO FITTIZIO

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 #INIZIALIZZAZIONE MOTORE DI TEMPLATING: importo la libreria
 from fastapi.templating import Jinja2Templates
@@ -7,19 +9,26 @@ from fastapi.templating import Jinja2Templates
 app = FastAPI ()
 #con questa riga dico alla libreria dove trovare i file html
 #il punto indica cartella corrente, ma posso anche scrivere solo il nome
+app.mount("/static", StaticFiles(directory="static"), name = "static")
 templates = Jinja2Templates(directory = "./templates")
 
+product_list = [
+    {"name": "notebook LENOVO", "price": 2999.99, "location": "Cagliari"},
+    {"name": "samsung 500", "price": 500, "location": "Sestu"}
+]
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request ):
     """Renders the home page"""
     #MOTORE DI TEMPLATING, IL PRIMO PARAMETRO SI PASSA SEMPRE DI DEFAULT
     #IL SECONDO PARAMETRO INDICA IL NOME DEL FILE HTML DA CUI DEVE PESCARE IL CODICE
     #TERZO PARAMETRO DELLA TEMPLATE RESPONSE
-    context =  {"title": "HOME PAGE" ,
-                "content": "Welcome to the home page!"
-    }
 
-    return templates.TemplateResponse(request = request, name = "home.html", context = context)
+
+    return templates.TemplateResponse(request = request, name = "home.html", context = {"test": "Welcome to the store"})
+
+@app.get ("/products", response_class = HTMLResponse)
+def products (request: Request):
+    return templates.TemplateResponse(request = request, name = "products.html", context = {"product_list": product_list})
 
 
 
